ce629ddf0c12acde1f9b20e7912d1d5b6ce6b0ab
Craig Joly <craig@snowkeep.ca>
2025-05-22 17:37:18 -0400

    rows and matrix corrected for rev4

--- keyboards/splitkb/halcyon/kyria/keymaps/snowkeep-hd_gold/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/snowkeep-hd_gold/keymap.c
@@ -17,33 +17,39 @@
 #include QMK_KEYBOARD_H
 #include "snowkeep-hd.h"
 
+// note the extra row for 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_HD] = LAYOUT(
+    [_HD] = LAYOUT_split_3x6_5_hlc(
      KC_TAB,  KC_J,    KC_G,    KC_M,    KC_P,    KC_V,                                          KC_COLN, KC_DOT,  KC_SLSH, KC_DQT,  KC_QUOT, KC_BSPC,
      KC_ESC,  GUI_R,   ALT_S,   CTR_N,   SFT_D,   KC_B,                                          KC_COMM, SFT_A,   CTR_E,   ALT_I,   GUI_H,   KC_UNDS,
      KC_DEL,  KC_X,    AGR_F,   KC_L,    KC_C,    KC_W,    KC_HOME, XXXXXXX,   XXXXXXX, KC_END,  KC_MINS, KC_U,    KC_O,    KC_Y,    KC_K,    M_BRACES,
-                              KC_INT3,  KC_NUM,   NAV,     KC_ENT,  KC_SPC,    KC_T,    SYM,     FUNC,    KC_NUM,  XXXXXXX
+                              KC_INT3,  KC_NUM,   NAV,     KC_ENT,  KC_SPC,    KC_T,    SYM,     FUNC,    KC_NUM,  XXXXXXX,
+     KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
-    [_SYM] = LAYOUT(
+    [_SYM] = LAYOUT_split_3x6_5_hlc(
      RGB_TOG, KC_GRV,  KC_LT,   KC_GT,   KC_DQT,  KC_DOT,                                        KC_AMPR, KC_UNDS, KC_LBRC, KC_RBRC, KC_PERC, _______,
      RGB_MOD, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL,  KC_HASH,                                       KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_QUES, XXXXXXX,
      _______, KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, M_UPDIR, _______, XXXXXXX,   XXXXXXX, _______, KC_TILD, KC_DLR,  KC_LCBR, KC_RCBR, KC_AT,   _______,
-                                _______, _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______
+                                _______, _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______,
+     KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
-    [_NAV] = LAYOUT(
+    [_NAV] = LAYOUT_split_3x6_5_hlc(
      _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP,                                       XXXXXXX, KC_7,    KC_8,    KC_9,    KC_MINS, _______,
      KC_DEL,  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                       XXXXXXX, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
      BL_STEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_EQL,  _______,
-                                _______, _______, _______, _______, _______,   _______, XXXXXXX, _______, KC_0,    KC_DOT
+                                _______, _______, _______, _______, _______,   _______, XXXXXXX, _______, KC_0,    KC_DOT,
+     KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
-    [_FUNC] = LAYOUT(
+    [_FUNC] = LAYOUT_split_3x6_5_hlc(
      QK_BOOT, XXXXXXX, KC_MPRV, KC_VOLU, KC_MNXT, XXXXXXX,                                       XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
      KC_DEL,  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                       XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______,
      BL_STEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______,
-                                _______, _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______
+                                _______, _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______,
+     KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
 
